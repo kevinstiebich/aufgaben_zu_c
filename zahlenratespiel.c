4e79f5693deb61de20da5c312a663fb812ecf2a0
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-15 16:46:22 +0200

    Lösen der Aufgabe

--- zahlenratespiel.c
+++ zahlenratespiel.c
@@ -1,1 +1,16 @@
 #include <stdio.h>
+
+int main() {
+    int geheim = 3, eingabe;
+    printf("Gebe eine Zahl ein: ");
+    scanf("%d", &eingabe);
+    
+    if (eingabe == geheim) {
+        printf("Korrekt!");
+    } else if (eingabe >= geheim) {
+        printf("Deine Zahl ist zu groß!\n");
+    } else {
+        printf("Deine Zahl ist zu klein!\n");
+    }
+    return 0;
+}
