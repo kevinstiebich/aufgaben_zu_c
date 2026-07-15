4e568902f1c6828b754980c99e9febb67b5ea165
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-15 16:19:16 +0200

    Aufgabe lösen

---
+++ wahrheitstabellen.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("A | B | A OR B\n");
+    printf("-----------\n");
+    printf("0 | 0 |   0\n");
+    printf("0 | 1 |   1\n");
+    printf("1 | 0 |   1\n");
+    printf("1 | 1 |   1\n");
+    return 0;
+}
