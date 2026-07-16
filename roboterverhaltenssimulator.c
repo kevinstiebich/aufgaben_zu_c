d0358134d5c7259927ab31e838ac79f172d408bc
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-16 10:27:21 +0200

    Ungültige Eingabe und Zustand 1 hinzufügen

--- roboterverhaltenssimulator.c
+++ roboterverhaltenssimulator.c
@@ -7,13 +7,21 @@ int main() {
     printf("Gebe einen Befehl ein (1 = blink, 2 = spin, 3 = beep): ");
     scanf("%d", &befehl);
 
-    if (zustand == 1 && befehl == 1) {
-        printf("Der Roboter zwinkert dir zu!");
-        return 0;
-    } else if (zustand == 1 && befehl == 2) {
-        printf("Der Roboter tanzt wie eine Ballerina!");
-        return 0;
-    } else if (zustand == 1 && befehl == 3) {
-        printf("Der Roboter piept die Melodie von Funky Town für dich!")
+    if (befehl > 3) {
+        printf("Üngültige Eingabe.\n");
+        return 1;
+    }
+
+    if (zustand == 1) {
+        if (befehl == 1) {
+            printf("Der Roboter zwinkert dir zu!\n");
+            return 0;
+        } else if (befehl == 2) {
+            printf("Der Roboter tanzt wie eine Ballerina!\n");
+            return 0;
+        } else {
+            printf("Der Roboter piept die Melodie von Funky Town für dich!\n");
+            return 0;
+        }
     }
 }
