69765b5e58ea21a8395fb73a1558b8d54f50dec7
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-16 12:13:32 +0200

    Variablen hinzufügen

--- wahrheitstabellen.c
+++ wahrheitstabellen.c
@@ -1,11 +1,46 @@
 #include <stdio.h>
 
 int main() {
+    int a, b, a_or_b;
+
     printf("A | B | A OR B\n");
     printf("-----------\n");
-    printf("0 | 0 |   0\n");
-    printf("0 | 1 |   1\n");
-    printf("1 | 0 |   1\n");
-    printf("1 | 1 |   1\n");
+
+    a = 0, b = 0;
+    if (a == 1 || b == 1) {
+        a_or_b = 1;
+        printf("%d | %d |   %d\n", a, b, a_or_b);
+    } else {
+        a_or_b = 0;
+        printf("%d | %d |   %d\n", a, b, a_or_b);
+    }
+
+    a = 0, b = 1;
+    if (a == 1 || b == 1) {
+        a_or_b = 1;
+        printf("%d | %d |   %d\n", a, b, a_or_b);
+    } else {
+        a_or_b = 0;
+        printf("%d | %d |   %d\n", a, b, a_or_b);
+    }
+
+    a = 1, b = 0;
+    if (a == 1 || b == 1) {
+        a_or_b = 1;
+        printf("%d | %d |   %d\n", a, b, a_or_b);
+    } else {
+        a_or_b = 0;
+        printf("%d | %d |   %d\n", a, b, a_or_b);
+    }
+
+    a = 1, b = 1;
+    if (a == 1 || b == 1) {
+        a_or_b = 1;
+        printf("%d | %d |   %d\n", a, b, a_or_b);
+    } else {
+        a_or_b = 0;
+        printf("%d | %d |   %d\n", a, b, a_or_b);
+    }
+
     return 0;
 }
