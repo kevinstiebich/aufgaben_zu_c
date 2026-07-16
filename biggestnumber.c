76c9d06f02656cab53187530206db3fe4cb0b257
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-16 10:56:44 +0200

    Verschönerungen hinzufügen: nicht in jeder If-Abfrage return

--- biggestnumber.c
+++ biggestnumber.c
@@ -12,17 +12,13 @@ int main() {
 
     if (zahl1 == zahl2 && zahl1 == zahl3) {
         printf("Alle Zahlen sind gleichgroß!\n");
-        return 0;
-    }
-
-    if (zahl1 >= zahl2 && zahl1 >= zahl3) {
+    } else if (zahl1 >= zahl2 && zahl1 >= zahl3) {
         printf("Die größte Zahl ist: %d\n", zahl1);
-        return 0;
     } else if (zahl2 >= zahl1 && zahl2 >= zahl3) {
         printf("Die größte Zahl ist: %d\n", zahl2);
-        return 0;
     } else {
         printf("Die größte Zahl ist: %d\n", zahl3);
-        return 0;
     }
+    
+    return 0;
 }
