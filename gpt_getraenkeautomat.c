bbe0fde8fa7f285b80276b72489c147138d02b1c
kevinstiebich <kevin.stiebich@gmail.com>
2026-07-17 12:31:07 +0200

    kl aenderungen

--- gpt_getraenkeautomat.c
+++ gpt_getraenkeautomat.c
@@ -7,19 +7,29 @@ int main() {
 
     while (moredrinks == 'y') {
         printf("Wähle ein Getränk (K/T/W): ");
-        scanf("%s", &drink);
+        scanf(" %c", &drink);
+        while (drink != 'K' && drink != 'k' && drink != 'T' && drink != 't' && drink != 'W' && drink != 'w') {
+            printf("Ungültige Eingabe. Bitte wähle K, T oder W: ");
+            scanf(" %c", &drink);
+        }
+        
         printf("Anzahl: ");
         scanf("%d", &anzahl);
+        while (anzahl <= 0) {
+            printf("Du kannst hier keine Drinks verkaufen! Neue Eingabe: ");
+            scanf("%d", &anzahl);
+        }
+
         
-        if (drink == 'K') {
+        if (drink == 'K' || drink == 'k') {
             endpreis = endpreis + preisK * anzahl;
-        } else if (drink == 'T') {
+        } else if (drink == 'T' || drink == 't') {
             endpreis = endpreis + preisT * anzahl;
-        } else if (drink == 'W') {
+        } else if (drink == 'W' || drink == 'w') {
             endpreis = endpreis + preisW * anzahl;
         }
         printf("Darf es noch etwas sein? (y/n) ");
-        scanf("%s", &moredrinks);
+        scanf(" %c", &moredrinks);
     }
 
     printf("Gesamtpreis: %.2f\n", endpreis);
